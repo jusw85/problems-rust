98278bc8537a2a1406db18b75b93e2d5efc74733
Justin Wong <jusw85@hotmail.com>
2020-12-28 23:17:03 +0800

    Move Day 11 lib

--- aoc2019/src/bin/day11.rs
+++ aoc2019/src/bin/day11.rs
@@ -82,9 +82,7 @@ use std::sync::mpsc;
 
 use anyhow::Context;
 
-use aoc2019::Direction;
-use aoc2019::Point;
-
+use crate::geom::{Direction, Point};
 use crate::prog::{Prog, Reply};
 
 type Result<T> = std::result::Result<T, anyhow::Error>;
@@ -173,6 +171,78 @@ fn paint(nums: &Vec<i64>,
     }
     thread.join().unwrap()?;
     Ok((grid, painted))
+}
+
+mod geom {
+    use std::ops::{Add, AddAssign};
+
+    #[derive(Eq, PartialEq, Hash, Copy, Clone, Default, Debug)]
+    pub struct Point {
+        pub y: i32,
+        pub x: i32,
+    }
+
+    impl Point {
+        pub fn new(y: i32, x: i32) -> Point {
+            Point { y, x }
+        }
+
+        #[allow(dead_code)]
+        pub fn add(&mut self, v: Vector) {
+            self.y += v.dy;
+            self.x += v.dx;
+        }
+    }
+
+    impl Add<Vector> for Point {
+        type Output = Self;
+
+        fn add(self, vector: Vector) -> Self {
+            Self {
+                y: self.y + vector.dy,
+                x: self.x + vector.dx,
+            }
+        }
+    }
+
+    impl AddAssign<Vector> for Point {
+        fn add_assign(&mut self, vector: Vector) {
+            *self = Self {
+                y: self.y + vector.dy,
+                x: self.x + vector.dx,
+            };
+        }
+    }
+
+    #[derive(Eq, PartialEq, Hash, Copy, Clone, Default, Debug)]
+    pub struct Vector {
+        pub dy: i32,
+        pub dx: i32,
+    }
+
+    impl Vector {
+        pub fn new(dy: i32, dx: i32) -> Vector {
+            Vector { dy, dx }
+        }
+    }
+
+    pub enum Direction {
+        N,
+        E,
+        S,
+        W,
+    }
+
+    impl Direction {
+        pub fn dydx(&self) -> Vector {
+            match self {
+                Direction::N => Vector::new(-1, 0),
+                Direction::E => Vector::new(0, 1),
+                Direction::S => Vector::new(1, 0),
+                Direction::W => Vector::new(0, -1),
+            }
+        }
+    }
 }
 
 mod prog {
--- aoc2019/src/lib.rs
+++ aoc2019/src/lib.rs
@@ -1,68 +1,68 @@
-use std::ops::{Add, AddAssign};
-
-#[derive(Eq, PartialEq, Hash, Copy, Clone, Default, Debug)]
-pub struct Point {
-    pub y: i32,
-    pub x: i32,
-}
-
-impl Point {
-    pub fn new(y: i32, x: i32) -> Point {
-        Point { y, x }
-    }
-
-    pub fn add(&mut self, v: Vector) {
-        self.y += v.dy;
-        self.x += v.dx;
-    }
-}
-
-impl Add<Vector> for Point {
-    type Output = Self;
-
-    fn add(self, vector: Vector) -> Self {
-        Self {
-            y: self.y + vector.dy,
-            x: self.x + vector.dx,
-        }
-    }
-}
-
-impl AddAssign<Vector> for Point {
-    fn add_assign(&mut self, vector: Vector) {
-        *self = Self {
-            y: self.y + vector.dy,
-            x: self.x + vector.dx,
-        };
-    }
-}
-
-#[derive(Eq, PartialEq, Hash, Copy, Clone, Default, Debug)]
-pub struct Vector {
-    pub dy: i32,
-    pub dx: i32,
-}
-
-impl Vector {
-    pub fn new(dy: i32, dx: i32) -> Vector {
-        Vector { dy, dx }
-    }
-}
-
-pub enum Direction {
-    N,
-    E,
-    S,
-    W,
-}
-
-impl Direction {
-    pub fn dydx(&self) -> Vector {
-        match self {
-            Direction::N => Vector::new(-1, 0),
-            Direction::E => Vector::new(0, 1),
-            Direction::S => Vector::new(1, 0),
-            Direction::W => Vector::new(0, -1),
-        }
-    }
-}
+// use std::ops::{Add, AddAssign};
+//
+// #[derive(Eq, PartialEq, Hash, Copy, Clone, Default, Debug)]
+// pub struct Point {
+//     pub y: i32,
+//     pub x: i32,
+// }
+//
+// impl Point {
+//     pub fn new(y: i32, x: i32) -> Point {
+//         Point { y, x }
+//     }
+//
+//     pub fn add(&mut self, v: Vector) {
+//         self.y += v.dy;
+//         self.x += v.dx;
+//     }
+// }
+//
+// impl Add<Vector> for Point {
+//     type Output = Self;
+//
+//     fn add(self, vector: Vector) -> Self {
+//         Self {
+//             y: self.y + vector.dy,
+//             x: self.x + vector.dx,
+//         }
+//     }
+// }
+//
+// impl AddAssign<Vector> for Point {
+//     fn add_assign(&mut self, vector: Vector) {
+//         *self = Self {
+//             y: self.y + vector.dy,
+//             x: self.x + vector.dx,
+//         };
+//     }
+// }
+//
+// #[derive(Eq, PartialEq, Hash, Copy, Clone, Default, Debug)]
+// pub struct Vector {
+//     pub dy: i32,
+//     pub dx: i32,
+// }
+//
+// impl Vector {
+//     pub fn new(dy: i32, dx: i32) -> Vector {
+//         Vector { dy, dx }
+//     }
+// }
+//
+// pub enum Direction {
+//     N,
+//     E,
+//     S,
+//     W,
+// }
+//
+// impl Direction {
+//     pub fn dydx(&self) -> Vector {
+//         match self {
+//             Direction::N => Vector::new(-1, 0),
+//             Direction::E => Vector::new(0, 1),
+//             Direction::S => Vector::new(1, 0),
+//             Direction::W => Vector::new(0, -1),
+//         }
+//     }
+// }
